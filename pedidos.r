7cc05e2045e33b4ccb718ab6eb8ef659af3b5af2
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2019-12-11 16:20:44 -0300

    Update pedidos.r

--- pedidos.r
+++ pedidos.r
@@ -50,12 +50,67 @@ p <- plot_ly(data, x = ~orgaos, y = ~tramitacao, type = 'bar', name = 'tramitaca
 stop_words_portuguese <- NULL
 stop_words_portuguese$word <- stopwords("portuguese")
 
+# Adicionando stop words
+custom_stop_words_portuguese <- tribble(
+  ~word,
+  "xxxxx", 
+  "é", 
+  "2017", 
+  "2", 
+  "2018", 
+  "xxxxx", 
+  "solicito", 
+  "prefeitura", 
+  "gostaria", 
+  "informações"
+)
+
+stop_words_portuguese2 <- stop_words_portuguese %>% 
+  rbind(custom_stop_words)
+
+# Tokenizar e remover stop words
 tidy_pedidos <- pedidos2018 %>%
   unnest_tokens(word, dc_pedido) %>%
-  anti_join(as.data.frame(stop_words_portuguese), by = c("word" = "word"))
+  anti_join(as.data.frame(stop_words2), by = c("word" = "word"))
 
+# Contar palavras
 contagem <- tidy_pedidos %>%
   count(word) %>%
   arrange(desc(n)) %>%
   top_n(50, n) %>%
   mutate(word2 = fct_reorder(word, n))
+
+# Plotar contagem de palavras
+ggplot(contagem, aes(x = word2, n)) +
+  geom_col() +
+  coord_flip() +
+  labs(
+    title = "Contagem de Palavras",
+    subtitle = "Contagem de Palavras Geral",
+    x = "Palavras",
+    y = "Contagem"
+  )
+
+# Contar palavras por órgão
+contagem_orgao <- tidy_pedidos %>%
+  filter(cd_orgao %in% c(67, 16, 10)) %>%
+  count(word, orgao_sigla) %>%
+  group_by(orgao_sigla) %>%
+  arrange(desc(n)) %>%
+  top_n(50, n) %>%
+  ungroup() %>% 
+  mutate(word2 = fct_reorder(word, n))
+
+
+# Plotar contagem de palavras
+ggplot(contagem_orgao, aes(x = word2, n, fill = orgao_sigla)) +
+  geom_col() +
+  facet_wrap(~ orgao_sigla, scales = "free_y") +
+  coord_flip() +
+  labs(
+    title = "Contagem de Palavras",
+    subtitle = "Contagem de Palavras Geral",
+    x = "Palavras",
+    y = "Contagem"
+  )
+
