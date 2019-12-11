a27c4108b7d49d8ff271cfb7f778105fbd55dc0b
Ricardo Reis <ricardo.l.b.reis@gmail.com>
2019-12-11 11:31:49 -0300

    Update pedidos.r

--- pedidos.r
+++ pedidos.r
@@ -2,8 +2,11 @@ library(plotly)
 library(dplyr)
 library(lubridate)
 library(stringr)
+library(tm)
+library(tidytext)
+library(forcats)
 
-pedidos2018 <- read.csv("~/Desktop/Analises/pedidorespondido2018atualizado.csv", sep=";", comment.char="#", stringsAsFactors = FALSE , encoding = "utf-8")
+pedidos2018 <- read.csv("~/Desktop/Analises/pedidos-sp/pedidorespondido2018atualizado.csv", sep=";", comment.char="#", stringsAsFactors = FALSE , encoding = "utf-8")
 glimpse(pedidos2018)
 
 ################################################################### TIDYING #########################################################################
@@ -17,13 +20,42 @@ pedidos2018$dt_resposta_atendimento <- str_replace_all(pedidos2018$dt_resposta_a
 pedidos2018$dt_resposta_atendimento = dmy_hm(pedidos2018$dt_resposta_atendimento)
 
 pedidos2018$orgao_sigla = str_split(pedidos2018$orgao_nome, '-', simplify=TRUE)[,1]
+
+########################## Pedidos Por Orgão
 pedidos2018_unicos <- pedidos2018[unique(pedidos2018$cd_pedido),]
 
 pedidos_x_orgao <- plot_ly(
-  x = names(tail(sort(table(pedido_unicos$orgao_sigla)),10)),
-  y = tail(sort(table(pedido_unicos$orgao_sigla)),10),
+  x = names(tail(sort(table(pedidos2018_unicos$orgao_sigla)),10)),
+  y = tail(sort(table(pedidos2018_unicos$orgao_sigla)),10),
   name = "Pedidos",
   type = "bar"
   ) %>%
   layout(title = 'Pedidos por Orgao')
 
+########################## Pedidos 
+tramitacao <- filter(pedidos2018, pedidos2018$status_nome == "Em tramitação")
+tramitacao <- table(tramitacao$orgao_nome)
+
+finalizado <- filter(pedidos2018, pedidos2018$status_nome == "Finalizado")
+finalizado <- table(finalizado$orgao_nome)
+orgaos <- names(finalizado)
+
+data <- data.frame(orgaos, tramitacao, finalizado)
+
+p <- plot_ly(data, x = ~orgaos, y = ~tramitacao, type = 'bar', name = 'tramitacao') %>%
+  add_trace(y = ~finalizado, name = 'finalizado') %>%
+  layout(yaxis = list(title = 'Count'), barmode = 'group')
+
+########################## Text Mining 
+stop_words_portuguese <- NULL
+stop_words_portuguese$word <- stopwords("portuguese")
+
+tidy_pedidos <- pedidos2018 %>%
+  unnest_tokens(word, dc_pedido) %>%
+  anti_join(as.data.frame(stop_words_portuguese), by = c("word" = "word"))
+
+contagem <- tidy_pedidos %>%
+  count(word) %>%
+  arrange(desc(n)) %>%
+  top_n(50, n) %>%
+  mutate(word2 = fct_reorder(word, n))
